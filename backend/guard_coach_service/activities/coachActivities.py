9ddf8be12b7383c530578867f028f08c657bf1a9
Karol Urbańczyk <kurbanczyk@ultrasinus.pl>
2023-07-21 17:39:01 +0200

    Get folks photos + model training init

---
+++ backend/guard_coach_service/activities/coachActivities.py
@@ -0,0 +1,52 @@
+import face_recognition
+import os
+import pickle
+import tempfile
+
+from flask import jsonify, make_response, request
+from guard_coach_service.dao.s3_client import S3Client
+from http import HTTPStatus
+from pathlib import Path
+
+DEFAULT_ENCODINGS_PATH = Path('encodings.pkl')
+
+class CoachActivities:
+    def __init__(self) -> None:
+        self.dao = S3Client()
+
+    def load_image_from_s3(self, folk_name: str, photo_name: str):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            local_file_path = os.path.join(temp_dir, os.path.basename(f'{folk_name}/{photo_name}'))
+            self.dao.download_folk_photo(folk_name, photo_name, local_file_path)
+
+            image = face_recognition.load_image_file(local_file_path)
+
+        return image
+
+    def train(self, model: str = 'hog') -> bool:
+        names = []
+        encodings = []
+
+        for folk_name in self.dao.list_existing_folks():
+            for folk_photo in self.dao.list_folk_photos(folk_name):
+                image_data = self.load_image_from_s3(folk_name, folk_photo['Key'])
+                face_encodings = face_recognition.face_encodings(image_data)
+
+                for encoding in face_encodings:
+                    names.append(folk_name)
+                    encodings.append(encoding)
+
+        name_encodings = {"names": names, "encodings": encodings}
+                
+        with DEFAULT_ENCODINGS_PATH.open(mode="wb") as f:
+            pickle.dump(name_encodings, f)
+
+        return make_response(
+            jsonify({
+                'message': 'Model accepted. Trainig has been completed!'
+            }),
+            HTTPStatus.ACCEPTED,
+            {
+                'Content-Type': 'application/json'
+            }
+        )
--- backend/guard_coach_service/dao/s3_client.py
+++ backend/guard_coach_service/dao/s3_client.py
@@ -3,7 +3,7 @@ from typing import List
 
 class S3Client:
     def __init__(self) -> None:
-        self.client = boto3.client(
+        self.client = boto3.client( # TODO: move to configuration
             's3',
             aws_access_key_id = 'minio',
             aws_secret_access_key = 'minio123',
@@ -18,6 +18,10 @@ class S3Client:
         except Exception:
             return False
 
+    def download_folk_photo(self, folk_name: str, photo_name: str, path: str):
+        self.client.download_file(folk_name, photo_name, path)
+        return True
+
     def list_existing_folks(self) -> List[str]:
         response = self.client.list_buckets()
 
@@ -25,6 +29,14 @@ class S3Client:
             return bucket['Name']
 
         return list(map(get_bucket_name, response['Buckets']))
+
+    def list_folk_photos(self, folk: str) -> List[str]:
+        response = self.client.list_objects_v2(Bucket = folk)
+
+        if 'Contents' in response:
+            return response['Contents']
+
+        return None
 
     def upload_folk_photo(self, bucket_name: str, image_content) -> bool:
         if not self.check_folk_exists(bucket_name):
